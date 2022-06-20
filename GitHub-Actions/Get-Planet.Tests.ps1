3f15dd6e2b2bd6c44290fc158a8407042a3ca255
David Boster <david.boster@medefis.com>
2022-06-20 11:20:37 -0500

    Add Get-Planet test from Pester quickstart

---
+++ GitHub-Actions/Get-Planet.Tests.ps1
@@ -0,0 +1,23 @@
+BeforeAll {
+    function Get-Planet ([string]$Name = '*') {        
+        $planets = @(
+            @{ Name = 'Mercury' }
+            @{ Name = 'Venus'   }
+            @{ Name = 'Earth'   }
+            @{ Name = 'Mars'    }
+            @{ Name = 'Jupiter' }
+            @{ Name = 'Saturn'  }
+            @{ Name = 'Uranus'  }
+            @{ Name = 'Neptune' }
+        ) | ForEach-Object { [PSCustomObject] $_ }
+
+        $planets | Where-Object { $_.Name -like $Name }
+    }
+}
+
+Describe 'Get-Planet' {
+    It 'Given no parameters, it lists all 8 planets' {
+        $allPlanets = Get-Planet
+        $allPlanets.Count | Should -Be 8
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,12 @@
+
+# Azure-Powershell: Examples & Sandboxes
+
+## CI/CD for PowerShell
+How can we do a CI/CD for PowerShell scripts?
+
+Path: `./github-actions'
+* Testing whether we can use GitHub actions to test
+
+### Resources
+- [GitHub - Automating and Testing PowerShell](https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-powershell)
+- [Pester QuickStart](https://pester.dev/docs/quick-start)
